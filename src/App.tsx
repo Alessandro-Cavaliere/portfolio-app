ed394e76efc2d0068c8e3ea0d0232e27473586a3
Alessandro-Cavaliere <81223433+Alessandro-Cavaliere@users.noreply.github.com>
2022-10-30 11:54:35 +0100

    Momentary changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import foto from './assets/foto.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={foto} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Personal Portfolio React App - Template: Typescript <3
         </p>
         <a
           className="App-link"
