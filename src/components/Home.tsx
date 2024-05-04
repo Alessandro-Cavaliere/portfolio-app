0bbf4689ba43f5ad9337f96e03e9563dbfea514c
Alessandro-Cavaliere <81223433+Alessandro-Cavaliere@users.noreply.github.com>
2024-05-04 04:58:37 +0200

    style: :zap: Refactor of Home's code for better view

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,42 @@
 import { useInView } from 'react-intersection-observer';
 import HomeCSS from "../styles/home.module.css"
 import mePhoto from "../assets/me.png"
+import { IoCubeOutline } from "react-icons/io5";
 import mouseScroll from "../assets/mouse_scroll.gif"
 import Navbar from './Navbar';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+const DEFAULT_SCROLL_GAP = 80
 
 export default function Home(){
     const { ref: containerChange , inView: containerChangeIsVisible, entry} = useInView();
+    const [scrollPosition, setScrollPosition] = useState(0);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
+    var scrollTop = document.documentElement.scrollTop * 0.5;
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentPosition = window.scrollY;
+            setScrollPosition(currentPosition);
+        };
+
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    useEffect(()=>{
+        console.log(scrollPosition)
+    })
     return(
         <>
         {!containerChangeIsVisible?
@@ -16,9 +45,11 @@ export default function Home(){
             <div className={`${ HomeCSS.container} ${containerChangeIsVisible? HomeCSS.container_variation: ''}`}>
                 <div className={HomeCSS.container__children}>
                     
-                    <h1><span className={HomeCSS.container__h1}>Welcome.</span></h1>
-                    <h2 className={HomeCSS.h2}>My name is <span className={HomeCSS.container__h2}>Alessandro Cavaliere</span>, 
-                    Blockchain Developer and master student in cybersecurity. This is my personal web site, enjoy it!</h2>
+                    <h2 className={HomeCSS.h2} style={{ transform: `translateX(-${scrollTop}px) translateY(-${scrollTop}px`, transition: 'transform 1s ease' }}>Cybersecurity</h2>
+                    <h2 className={`${HomeCSS.h2} ${HomeCSS.variant} ${containerChangeIsVisible ? HomeCSS.moveRight : ''}`} style={{ transform: `translateX(+${scrollTop}px) translateY(-${scrollTop}px`, transition: 'transform 2s ease' }}>Student <IoCubeOutline className={HomeCSS.block} size={80} color='#8ca0b2'/></h2>
+                    <h2 className={`${HomeCSS.h2} ${containerChangeIsVisible ? HomeCSS.moveLeft : ''}`} style={{ transform: `translateX(-${scrollTop}px) translateY(-${scrollTop}px`, transition: 'transform 2s ease' }}>& Blockchain</h2>
+                    <h2 className={`${HomeCSS.h2} ${HomeCSS.variant}  ${containerChangeIsVisible ? HomeCSS.moveRight : ''}`} style={{ transform: `translateX(+${scrollTop}px) translateY(-${scrollTop}px`, transition: 'transform 2s ease' }}>Developer</h2>
+                    <h2 className={HomeCSS.paragraph} style={{ transform: `translateX(-${scrollTop}px) translateY(-${scrollTop}px`, transition: 'transform 2s ease' }}>Based in <span style={{ color:'#00a9006b'}}>It</span><span>a</span><span style={{color:'#dc00007d',marginLeft:3}}>ly</span></h2>
                     <div className={HomeCSS.div_mouse_style}>
                             <div className={HomeCSS.mousey}>
                                 <div className={HomeCSS.scroller}></div>
@@ -44,11 +75,11 @@ export default function Home(){
              <Navbar  navbarScroll={containerChangeIsVisible}/>
             <div className={`${ HomeCSS.container} ${HomeCSS.container_variation}`}>
                 <div className={HomeCSS.container__children}>
-                    
-                    <h1 className={`${ HomeCSS.container__h1} ${ HomeCSS.container__h1_variation}`}>Welcome.</h1>
-                    <h2 className={`${ HomeCSS.container__h2} ${ HomeCSS.container__h2_variation}`}>My name is <span className={HomeCSS.container__h2}>Alessandro Cavaliere</span>, 
-                    Blockchain Developer and master student in cybersecurity. This is my personal web site, enjoy it!</h2>
-            
+                    <h2 className={HomeCSS.h2} style={{ transform: `translateX(-${scrollTop}px) translateY(-${scrollTop}px)`, transition: 'transform 0.5s ease' }}>Cybersecurity</h2>
+                    <h2 className={`${HomeCSS.h2} ${HomeCSS.variant} ${containerChangeIsVisible ? HomeCSS.moveRight : ''}`} style={{  transform: `translateX(+${scrollTop}px) translateY(-${scrollTop}px`, transition: 'transform 0.5s ease' }}>Student <IoCubeOutline className={HomeCSS.block} size={80} color='#8ca0b2'/></h2>
+                    <h2 className={`${HomeCSS.h2} ${containerChangeIsVisible ? HomeCSS.moveLeft : ''}`} style={{ transform: `translateX(-${scrollTop}px) translateY(-${scrollTop}px`, transition: 'transform 0.5s ease'}}>& Blockchain</h2>
+                    <h2 className={`${HomeCSS.h2} ${HomeCSS.variant}  ${containerChangeIsVisible ? HomeCSS.moveRight : ''}`} style={{ transform: `translateX(+${scrollTop}px) translateY(-${scrollTop}px`, transition: 'transform 0.5s ease'}}>Developer</h2>
+                    <h2 className={HomeCSS.paragraph} style={{  transform: `translateX(-${scrollTop}px) translateY(-${scrollTop}px`, transition: 'transform 0.5s ease'}}>Based in <span style={{ color:'#00a9006b'}}>It</span><span>a</span><span style={{color:'#dc00007d',marginLeft:3}}>ly</span></h2>
                 </div>
                 <img src={mePhoto} className={HomeCSS.image_style_variation}/>
             </div>
