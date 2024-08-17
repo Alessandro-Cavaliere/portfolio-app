4802b9f3b556968ecb713637780e4a7b588efe09
Alessandro Cavaliere <alessandrocavaliere41@gmail.com>
2024-08-17 08:10:56 +0200

    refactor: :art: Refactoring "App.tsx" to define component display order

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import './app.scss'
 import Navbar from './components/navbar/Navbar';
 import Cursor from './components/cursor/Cursor';
+import PotraitView from './components/potraitview/PortraitView';
+import Parallax from './components/parallax/Parallax';
+import Projects from './components/projects/Projects';
+import Contact from './components/contact/Contact';
 
 /**
  * App.tsx component -> main component of the application that renders all the other components
@@ -9,10 +13,26 @@ import Cursor from './components/cursor/Cursor';
 function App() {
   return (
     <>
-      <Cursor />
-      <section id="Homepage">
-        <Navbar />
-      </section>
+       <div>
+        <Cursor />
+        <section id="Homepage">
+          <Navbar />
+          <PotraitView />
+        </section>
+        {/* <section id="Services">
+          <Parallax type="services" />
+        </section>
+        <section>
+          <Services />
+        </section> */}
+        <section id="Projects">
+          <Parallax type="projects" />
+        </section>
+        <Projects />
+        <section id="Contact">
+          <Contact />
+        </section>
+      </div>
     </>
   )
 }
