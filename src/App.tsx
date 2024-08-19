fd51da46d4e6bb558c2c0bbc85b70bffcd55728a
Alessandro Cavaliere <alessandrocavaliere41@gmail.com>
2024-08-19 06:32:18 +0200

    feat: :sparkles: `Preloader.tsx` -> A Loader Page before rendering all the stuffs

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,55 @@
-import './app.scss'
+// App.tsx
+import { useState } from 'react';
+import './app.scss';
 import Navbar from './components/navbar/Navbar';
 import Cursor from './components/cursor/Cursor';
 import PotraitView from './components/potraitview/PortraitView';
 import Parallax from './components/parallax/Parallax';
 import Projects from './components/projects/Projects';
 import Contact from './components/contact/Contact';
+import Experiences from './components/experiences/Experiences';
+import Preloader from './components/preloader/Preloader';
 
 /**
  * App.tsx component -> main component of the application that renders all the other components
  * @returns JSX.Element that represents the main component of the application
  */
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  const handleLoadingComplete = () => {
+    setLoading(false); // Hide the preloader and show the content
+  };
+
   return (
     <>
-       <div>
-        <Cursor />
-        <section id="Homepage">
-          <Navbar />
-          <PotraitView />
-        </section>
-        {/* <section id="Services">
-          <Parallax type="services" />
-        </section>
-        <section>
-          <Services />
-        </section> */}
-        <section id="Projects">
-          <Parallax type="projects" />
-        </section>
-        <Projects />
-        <section id="Contact">
-          <Contact />
-        </section>
-      </div>
+      {loading ? (
+        <Preloader onComplete={handleLoadingComplete} />
+      ) : (
+        <div className="app-content">
+          <Cursor />
+          <section id="Homepage">
+            <Navbar />
+            <PotraitView />
+          </section>
+          <section id="Experiences">
+            <Parallax type="experiences" />
+          </section>
+          <section>
+            <Experiences />
+          </section>
+          <section id="Projects">
+            <Parallax type="projects" />
+          </section>
+          <Projects />
+          <section id="Contact">
+            <Contact />
+            <p className="copyright">Copyright © 2024 All copyrights reserved to Alessandro Cavaliere</p>
+          </section>
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/preloader/Preloader.tsx
@@ -0,0 +1,93 @@
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import "./preloader.scss";
+
+interface PreloaderProps {
+    onComplete: () => void;
+  }
+
+/**
+ * Preloader.tsx component -> Preload page for the website
+ * @returns JSX.Element that represents the preloader component
+ */
+const Preloader = ({ onComplete }: PreloaderProps) => {
+    /*//////////////////////////////////////////////////////////////
+                         STATE DECLARATIONS
+    //////////////////////////////////////////////////////////////*/
+    const [loading, setLoading] = useState(true);
+    const [progress, setProgress] = useState(0);
+  
+    /*//////////////////////////////////////////////////////////////
+                             USE EFFECT
+    //////////////////////////////////////////////////////////////*/
+    useEffect(() => {
+      const duration = 5000; 
+      const interval = 50; 
+      const totalSteps = duration / interval;
+  
+      const timer = setTimeout(() => {
+        setLoading(false);
+        onComplete();
+      }, duration);
+  
+      const progressInterval = setInterval(() => {
+        setProgress((prevProgress) => {
+          const nextProgress = prevProgress + (100 / totalSteps);
+          return nextProgress >= 100 ? 100 : nextProgress;
+        });
+      }, interval);
+      return () => {
+        clearTimeout(timer);
+        clearInterval(progressInterval);
+      };
+    }, [onComplete]);
+
+ 
+    /*//////////////////////////////////////////////////////////////
+                            OBJECTS FOR FRAMER MOTION
+    //////////////////////////////////////////////////////////////*/
+    const blockVariants = {
+        rotate: {
+        rotate: [0, 360], 
+        transition: {
+            duration: 2, 
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "loop",
+        },
+        },
+    };
+
+/*//////////////////////////////////////////////////////////////
+                         STATE DECLARATIONS
+//////////////////////////////////////////////////////////////*/
+return (
+    loading ? (
+        <div className="preloader">
+
+            <progress className="progress-bar" value={progress} max="100" />
+            <div className="child-div-preloader">   
+            {Array(3).fill(null).map((_, index) => (
+                <motion.svg
+                    key={index}
+                    viewBox="0 0 512 512"
+                    id="block"
+                    className="block"
+                    variants={blockVariants as never}
+                    animate={{ rotate: [0, 360], transition: { duration: 1.5, ease: "easeInOut", repeat: Infinity, repeatType: "loop", times: [0, 0.8] } }}
+                >
+                    <polygon fill="#e0e0e0" points="34.29 384 34.3 128 256 256 256 512 34.29 384"></polygon>
+                    <polyline fill="#c0c0c0" points="34.29 128 256 0 477.7 128 256 256"></polyline>
+                    <polyline fill="#b0b0b0" points="256 256 477.71 128 477.71 384 256 512"></polyline>
+                </motion.svg>
+            ))}
+            </div>
+            <p className="loading-text">
+                <span>My </span> <span>portfolio</span> <span>is</span> <span>powered</span> <span>by</span> <span className="highlight">IPFS</span> <span>and</span> <span className="highlight">ESN</span> <span>,</span> <span>embracing</span> <span>decentralized</span> <span>hosting</span> <span>for</span> <span>a</span> <span>modern</span> <span>web</span> <span>experience!</span>
+            </p>
+        </div>
+    ) : null
+);
+};
+
+export default Preloader;
