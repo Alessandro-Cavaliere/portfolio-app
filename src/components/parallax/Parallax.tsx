01e6c2abb773143a80558ca9c7db65ad8e9239e3
Alessandro-Cavaliere <alessandrocavaliere41@gmail.com>
2024-09-04 23:49:14 +0200

    fix: :zap: Fixed the density of `particles` in the experience section

--- src/components/parallax/Parallax.tsx
+++ src/components/parallax/Parallax.tsx
@@ -7,7 +7,7 @@ import type { Container, Engine } from "tsparticles-engine";
 
 /**
  * Parallax.tsx component -> renders the parallax effect on the screen
- * @param type - type of parallax effect 
+ * @param type - type of parallax effect
  * @returns JSX.Element of the Parallax component
  */
 const Parallax = ({ type }: { type: string }) => {
@@ -24,18 +24,21 @@ const Parallax = ({ type }: { type: string }) => {
   const yText = useTransform(scrollYProgress, [0, 1], ["0%", "500%"]);
   const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
 
-  const particlesInit = useCallback(async (engine :Engine) => {
+  const particlesInit = useCallback(async (engine: Engine) => {
     console.log(engine);
     // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
     // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
     // starting from v2 you can add only the features you need reducing the bundle size
     //await loadFull(engine);
     await loadSlim(engine);
-}, []);
+  }, []);
 
-const particlesLoaded = useCallback(async (container: Container | undefined) => {
-  await console.log(container);
-}, []);
+  const particlesLoaded = useCallback(
+    async (container: Container | undefined) => {
+      await console.log(container);
+    },
+    []
+  );
 
   /*//////////////////////////////////////////////////////////////
                              RETURN
@@ -57,86 +60,84 @@ const particlesLoaded = useCallback(async (container: Container | undefined) => 
       <motion.div
         className="particles"
         style={{
-          y: yBg
+          y: yBg,
         }}
       >
-      <Particles 
-      id="tsparticles"
-      init={particlesInit}
-      loaded={particlesLoaded}
-      options={{
-          background: {
+        <Particles
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={{
+            background: {
               color: {
-                  value: "linear-gradient(180deg, rgb(9, 14, 17), rgb(47, 73, 90))",
+                value:
+                  "linear-gradient(180deg, rgb(9, 14, 17), rgb(47, 73, 90))",
               },
-          },
-          container:{
-            
-          },
-          fpsLimit: 180,
-          interactivity: {
+            },
+            container: {},
+            fpsLimit: 180,
+            interactivity: {
               events: {
-                  onHover: {
-                      enable: true,
-                      mode: "repulse",
-                  },
-                  resize: true,
+                onHover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+                resize: true,
               },
               modes: {
-                  push: {
-                      quantity: 4,
-                  },
-                  repulse: {
-                      distance: 200,
-                      duration: 0.4,
-                  }
+                push: {
+                  quantity: 4,
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 0.4,
+                },
               },
-          },
-          particles: {
+            },
+            particles: {
               color: {
-                  value: "#ffffff",
+                value: "#ffffff",
               },
               links: {
-                  color: "#ffffff",
-                  distance: 150,
-                  enable: true,
-                  opacity: 0.5,
-                  width: 1,
+                color: "#ffffff",
+                distance: 150,
+                enable: true,
+                opacity: 0.5,
+                width: 1,
               },
               move: {
-                  direction: "none",
-                  enable: true,
-                  outModes: {
-                      default: "bounce",
-                  },
-                  random: false,
-                  speed: 3,
-                  straight: false,
+                direction: "none",
+                enable: true,
+                outModes: {
+                  default: "bounce",
+                },
+                random: false,
+                speed: 3,
+                straight: false,
               },
               number: {
-                  density: {
-                      enable: true,
-                      area: 800,
-                  },
-                  value: 100,
+                density: {
+                  enable: true,
+                  area: 800,
+                },
+                value: 60,
               },
               opacity: {
-                  value: 0.5,
+                value: 0.5,
               },
               shape: {
-                  type: "circle",
+                type: "circle",
               },
               size: {
-                  value: { min: 1, max: 3 },
+                value: { min: 1, max: 3 },
               },
-          },
-          detectRetina: true,
-      }}
-  />
+            },
+            detectRetina: true,
+          }}
+        />
       </motion.div>
-      
+
       <motion.div style={{ x: yBg }} className="stars"></motion.div>
-      
     </div>
   );
 };
